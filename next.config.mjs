06aba4dff37981dc2775452b634e855cf5aebb42
ilunarivan <ilunarivan@gmail.com>
2026-03-26 09:34:55 +0000

    final update

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,14 @@
+// next.config.js
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "cdn.picsart.io",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
