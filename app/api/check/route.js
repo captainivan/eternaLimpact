ddacd3838e34a5a404241eee43c4bd2e3a17a664
ilunarivan <ilunarivan@gmail.com>
2026-04-01 14:27:33 +0000

    lests ee

---
+++ app/api/check/route.js
@@ -0,0 +1,36 @@
+
+
+export async function POST() {
+    let arr = [];
+    const KEYS = [
+        process.env.PICSART_API_KEY_1,
+        process.env.PICSART_API_KEY_2,
+        process.env.PICSART_API_KEY_3,
+        process.env.PICSART_API_KEY_4,
+        process.env.PICSART_API_KEY_5,
+        process.env.PICSART_API_KEY_6,
+        process.env.PICSART_API_KEY_7,
+        process.env.PICSART_API_KEY_8,
+        process.env.PICSART_API_KEY_9,
+        process.env.PICSART_API_KEY_10,
+        process.env.PICSART_API_KEY_11
+    ]
+    for (let i = 0; i < KEYS.length; i++) {
+        try {
+            const api = await fetch("https://genai-api.picsart.io/v1/balance", {
+                method: 'GET',
+                headers: {
+                    accept: 'application/json',
+                    'X-Picsart-API-Key': `${KEYS[i]}`
+                }
+            });
+            const data = await api.json();
+            console.log(data);
+            arr.push(data);
+        } catch (error) {
+            console.log(error);
+        }
+    }
+    console.log(arr);
+    return Response.json({arr})
+}
